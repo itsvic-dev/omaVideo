b410dad707687956e76f5c510d23798143c3e254
omame <me@omame.xyz>
2023-06-28 19:09:33 +0200

    going well but crashing on f_op->write

--- src/platform/linux-kernel.c
+++ src/platform/linux-kernel.c
@@ -1,11 +1,15 @@
+#include <asm-generic/fcntl.h>
 #include <asm/segment.h>
 #include <asm/uaccess.h>
 #include <core/core.h>
 #include <linux/buffer_head.h>
+#include <linux/delay.h>
+#include <linux/fb.h>
 #include <linux/fcntl.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/limits.h>
 #include <linux/module.h>
 #include <linux/notifier.h>
 #include <linux/panic.h>
@@ -15,6 +19,9 @@
 MODULE_DESCRIPTION("omaVideo - Linux kernel frontend");
 MODULE_AUTHOR("omame");
 MODULE_LICENSE("GPL");
+
+#define FILE_PATH "/tmp/video.bin"
+#define FB_WIDTH 1152 // i dont have a mechanism for detecting fb width lol!!
 
 void func_log(char *section, char *format, ...) {
   char buffer[256];
@@ -28,7 +35,7 @@ void func_log(char *section, char *format, ...) {
 struct file *filp = NULL;
 
 bool func_fopen(void) {
-  filp = filp_open("/tmp/video.bin", O_RDONLY, 0);
+  filp = filp_open(FILE_PATH, O_RDONLY, 0);
   if (IS_ERR(filp)) {
     filp = NULL;
     return FALSE;
@@ -60,16 +67,79 @@ int func_strncmp(const char *str1, const char *str2, u64 n) {
   return strncmp(str1, str2, n);
 }
 
+void *func_memset(void *dest, int value, u64 size) {
+  return memset(dest, value, size);
+}
+
+struct file *fbfp;
+int l_width;
+int l_height;
+u8 *display_buf;
+bool func_display_open(int width, int height) {
+  fbfp = filp_open("/dev/fb0", O_RDWR, 0);
+  l_width = width;
+  l_height = height;
+  display_buf = kzalloc(FB_WIDTH * height * 4, GFP_KERNEL);
+
+  if (IS_ERR(fbfp)) {
+    pr_warn("omaVideo: failed to open /dev/fb0");
+    return FALSE;
+  }
+
+  pr_warn(
+      "omaVideo: TODO: framebuffer width detection, using hardcoded value %d",
+      FB_WIDTH);
+  // fbfp->f_op->unlocked_ioctl(fbfp, FBIOGET_VSCREENINFO,
+  //                            (unsigned long)(&vinfo));
+  // if (vinfo.bits_per_pixel != 32) {
+  //   pr_info("omaVideo: framebuffer has %d BPP, expected 32",
+  //           vinfo.bits_per_pixel);
+  //   return FALSE;
+  // }
+
+  return TRUE;
+}
+void func_display_frame(u8 *frame_data) {
+  for (int y = 0; y < l_height; y++) {
+    for (int x = 0; x < l_width; x++) {
+      int idx = y * l_width + x;
+      display_buf[idx * 4] = frame_data[idx];
+    }
+  }
+  // kernel_write(fbfp, display_buf, FB_WIDTH * l_height * 4, 0);
+  fbfp->f_op->write(fbfp, display_buf, FB_WIDTH * l_height * 4, 0);
+}
+bool func_display_close(void) {
+  filp_close(fbfp, NULL);
+  return TRUE;
+}
+
+void func_msleep(unsigned long msecs) {
+  // msleep(msecs);
+}
+
+long long func_get_ms_time(void) {
+  // TODO
+  return 0;
+}
+
 struct omavideo_platform_funcs funcs = {
     .log = *func_log,
+    .msleep = *func_msleep,
+    .get_ms_time = *func_get_ms_time,
 
     .malloc = *func_malloc,
     .free = *func_free,
+    .memset = *func_memset,
     .strncmp = *func_strncmp,
 
     .fopen = *func_fopen,
     .fread = *func_fread,
     .fclose = *func_fclose,
+
+    .display_open = *func_display_open,
+    .display_frame = *func_display_frame,
+    .display_close = *func_display_close,
 };
 
 static int omavideo_panic_handler(struct notifier_block *self, unsigned long i,
